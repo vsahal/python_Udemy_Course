c3ca14b164141b9c0e9d6fa3145b7ed6601c758e
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-01-08 16:32:10 -0600

    Update queueImplementation.py
    removed trial code

--- stacks_queues_deques/queueImplementation.py
+++ stacks_queues_deques/queueImplementation.py
@@ -15,13 +15,3 @@ class Queue(object):
 
   def size(self):
     return len(self.items)
-
-
-q = Queue()
-q.size()
-print(q.enqueue(1))
-print(q.enqueue(2))
-print(q.isEmpty())
-print(q.dequeue())
-print(q.size())
-print(q())
