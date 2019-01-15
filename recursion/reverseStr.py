3c87bd85ac0f416deaec90abf32dd1384e2ff586
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-01-15 16:45:28 -0600

    Update reverseStr.py
    added a slightly different way to reverse a str still using recursion

--- recursion/reverseStr.py
+++ recursion/reverseStr.py
@@ -3,3 +3,11 @@ def reverseStr(s):
     return s[-1]
   else:
     return s[-1] + reverseStr(s[:-1])
+
+  #another way to do this
+  
+ def reverseStr1(s):
+  if len(s) == 1:
+    return s[0]
+  else:
+    return reverseStr1(s[1:]) + s[0]
