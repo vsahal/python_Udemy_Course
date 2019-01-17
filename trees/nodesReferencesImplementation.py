f18cd309439f6a129e981d6c869f7d31ad02c155
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-01-17 17:13:33 -0600

    Add files via upload
    implementation of object binary tree

---
+++ trees/nodesReferencesImplementation.py
@@ -0,0 +1,17 @@
+class BinaryTree(object):
+
+  def __init__(self, rootObj):
+    self.key = rootObj
+    self.leftChild = None
+    self.rightChild = None
+
+  def insertLeft(self, newNode):
+    if self.leftChild == None:
+      self.leftChild = BinaryTree(newNode)
+    else:
+      t = BinaryTree(newNode)
+      t.leftChild = self.leftChild
+      self.leftChild = t
+
+  def insertRight(self, newNode):
+    pass
