f87afc4520bf99cd4baa82bcf84a5df07b9db0c7
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-01-19 23:26:16 -0600

    Update uniqueChar.py
    added another method that could work

--- arrays/uniqueChar.py
+++ arrays/uniqueChar.py
@@ -23,3 +23,13 @@ def uniqueChar1(str):
             emptyStr += letters
     
     return True
+
+def uniqueChar2(str):
+    
+    str1 = set(str)
+    
+    for letters in str:
+        if letters in str1:
+            return False
+        
+        return True
